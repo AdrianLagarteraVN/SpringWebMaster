56c67daa472c2fee6b4aca198df275f311653867
Adrian Lagartera <alagartera@viewnext.com>
2020-09-22 18:04:28 +0200

    Conflicto

--- src/main/java/com/viewnext/curso/SpringWebApplication.java
+++ src/main/java/com/viewnext/curso/SpringWebApplication.java
@@ -15,7 +15,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringWebApplication {
 
-    //Hola
+    //Adios
 	public static void main(String[] args) {
 		SpringApplication.run(SpringWebApplication.class, args);
 	}
