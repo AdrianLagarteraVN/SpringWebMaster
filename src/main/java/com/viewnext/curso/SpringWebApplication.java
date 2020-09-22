deb215c2ea81371577f28a8ff4f72c4f2b3bc89a
AdrianLagarteraVN <70515381+AdrianLagarteraVN@users.noreply.github.com>
2020-09-22 18:03:24 +0200

    Update SpringWebApplication.java

--- src/main/java/com/viewnext/curso/SpringWebApplication.java
+++ src/main/java/com/viewnext/curso/SpringWebApplication.java
@@ -15,7 +15,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringWebApplication {
 
-    //Hola
+    //Nos vemos mañana
 	public static void main(String[] args) {
 		SpringApplication.run(SpringWebApplication.class, args);
 	}
